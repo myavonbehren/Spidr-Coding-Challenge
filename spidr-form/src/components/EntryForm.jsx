92fb8a1735e107923f5561ea0c96b246b56a39c6
Mya Von Behren <mvonbehr@depaul.edu>
2025-07-12 18:12:59 -0400

    refactor: update inputs to use handleChange function

--- spidr-form/src/components/EntryForm.jsx
+++ spidr-form/src/components/EntryForm.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import '../styles/EntryForm.css'
+import CurrencyInput from 'react-currency-input-field';
+
 
 export default function EntryForm() {
     const [formData, setFormData] = useState({
@@ -11,11 +13,24 @@ export default function EntryForm() {
         pin: ''
     });
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log('Form submitted');
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
     };
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Form submitted: ', formData);
+    };
+
+    const currencyInputStyle = {
+        paddingLeft: '28px'
+    };
+
+    
 
 
     return (
@@ -30,6 +45,7 @@ export default function EntryForm() {
                         name="firstName" 
                         placeholder="First Name"
                         value={formData.firstName}
+                        onChange={handleChange}
                         required 
                         className='input-field' />
                 </div>
@@ -39,6 +55,8 @@ export default function EntryForm() {
                         type="text" 
                         name="lastName" 
                         placeholder="Last Name" 
+                        value={formData.lastName}
+                        onChange={handleChange}
                         required 
                         className='input-field' />
                 </div>
@@ -47,7 +65,9 @@ export default function EntryForm() {
                     <input
                         type="tel" 
                         name="phone" 
-                        placeholder="Phone Number" 
+                        placeholder="Phone Number"
+                        value={formData.phone}
+                        onChange={handleChange}
                         required 
                         className='input-field' />
                 </div>
@@ -57,18 +77,28 @@ export default function EntryForm() {
                         type="email"
                         name="email"
                         placeholder="Email Address"
+                        value={formData.email}
+                        onChange={handleChange}
                         required
                         className='input-field' />
                 </div>
 
                 <div className='form-group'>
                     <label className='label'>Guess the Air Fryer's Cost</label>
-                    <input 
-                        type="text"
-                        name="airFryerCost"
-                        placeholder="0.00"
-                        required
-                        className='input-field' />
+                    <div className='currency-container'>
+                        <span className='dollar-sign'>$</span>
+                        <CurrencyInput
+                            className='input-field'
+                            id="input-field"
+                            name="airFryerCost"
+                            placeholder="0.00"
+                            defaultValue={10.00}
+                            decimalsLimit={2}
+                            required
+                            style={currencyInputStyle}
+                            onValueChange={(value, name, values) => console.log(value, name, values)}
+                            />
+                    </div>
                 </div>
 
                 <div className='form-group'>
@@ -77,6 +107,8 @@ export default function EntryForm() {
                         type="password"
                         name="pin"
                         placeholder="XXXX-XXXX-XXXX-XXXX"
+                        value={formData.pin}
+                        onChange={handleChange}
                         required
                         className='input-field' />
                 </div>
