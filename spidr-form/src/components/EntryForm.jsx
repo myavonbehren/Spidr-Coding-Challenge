5e5447ed97b075f113fed901c630d14191ef2bc1
Mya Von Behren <mvonbehr@depaul.edu>
2025-07-12 17:13:06 -0400

    feat: add name input

--- spidr-form/src/components/EntryForm.jsx
+++ spidr-form/src/components/EntryForm.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 
 export default function EntryForm() {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Form submitted');
+    };
+
     return (
       <div className='main-container'>
         <div className='form-container'>
             <h1>Spidr Air Fryer</h1>
             <h2>Interest Form</h2>
-            
+            <form onSubmit={handleSubmit}>
+                <div className='form-group'>
+                    <label>First Name</label>
+                    <input 
+                        type="text" 
+                        name="firstName" 
+                        placeholder="First Name" 
+                        required 
+                        className='input-field' />
+                </div>
+            </form>
+
         </div>
         
       </div>
