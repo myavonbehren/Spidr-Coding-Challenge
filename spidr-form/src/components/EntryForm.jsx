a36077ff04c8aadfccd39c7b88143861e9c905de
Mya Von Behren <mvonbehr@depaul.edu>
2025-07-12 17:09:08 -0400

    feat: add EntryForm component for user interest submission

---
+++ spidr-form/src/components/EntryForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function EntryForm() {
+    return (
+      <div className='main-container'>
+        <div className='form-container'>
+            <h1>Spidr Air Fryer</h1>
+            <h2>Interest Form</h2>
+            
+        </div>
+        
+      </div>
+    );
+  }
