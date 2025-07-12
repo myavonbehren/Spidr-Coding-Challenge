23de15c2f6bd42fd86452e2a96ebff455078c207
Mya Von Behren <mvonbehr@depaul.edu>
2025-07-12 16:17:35 -0400

    feat: update App component structure with additional headings and button

--- spidr-form/src/App.jsx
+++ spidr-form/src/App.jsx
@@ -4,7 +4,10 @@ function App() {
 
   return (
     <>
-      <h1>Spidr Air Fryer</h1>
+      <h1 >Spidr Air Fryer</h1>
+      <h2>Interest Form</h2>
+      <h3>Coming Soon!</h3>
+      <button>Test</button>
     </>
   )
 }
