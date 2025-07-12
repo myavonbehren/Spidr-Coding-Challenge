d8e86074dad91ab73e2735f12d4c92710d07c86f
Mya Von Behren <mvonbehr@depaul.edu>
2025-07-12 17:22:29 -0400

    feat: add all required input fields

--- spidr-form/src/components/EntryForm.jsx
+++ spidr-form/src/components/EntryForm.jsx
@@ -7,7 +7,7 @@ export default function EntryForm() {
     };
 
     return (
-      <div className='main-container'>
+      <div className='form-page'>
         <div className='form-container'>
             <h1>Spidr Air Fryer</h1>
             <h2>Interest Form</h2>
@@ -21,10 +21,61 @@ export default function EntryForm() {
                         required 
                         className='input-field' />
                 </div>
-            </form>
+                <div className='form-group'>
+                    <label>Last Name</label>
+                    <input 
+                        type="text" 
+                        name="lastName" 
+                        placeholder="Last Name" 
+                        required 
+                        className='input-field' />
+                </div>
+                <div className='form-group'>
+                    <label>Phone Number</label>
+                    <input
+                        type="tel" 
+                        name="phone" 
+                        placeholder="Phone Number" 
+                        required 
+                        className='input-field' />
+                </div>
+                <div className='form-group'>
+                    <label>Email Address</label>
+                    <input 
+                        type="email"
+                        name="email"
+                        placeholder="Email Address"
+                        required
+                        className='input-field' />
+                </div>
 
+                <div className='form-group'>
+                    <label>Guess the Air Fryer's Cost</label>
+                    <input 
+                        type="text"
+                        name="airFryerCost"
+                        placeholder="0.00"
+                        required
+                        className='input-field' />
+                </div>
+
+                <div className='form-group'>
+                    <label>16-Digit PIN</label>
+                    <input
+                        type="password"
+                        name="pin"
+                        placeholder="XXXX-XXXX-XXXX-XXXX"
+                        required
+                        className='input-field' />
+                </div>
+
+                <button 
+                    type="submit" 
+                    className='submit-button'>
+                    Submit
+                </button>
+            </form>
         </div>
-        
       </div>
     );
   }
